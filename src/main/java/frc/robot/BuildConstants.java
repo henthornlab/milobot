9c9035a73236bfe321d6eae66f9860b829abdcbd
kt-h <113950897+kt-h@users.noreply.github.com>
2025-01-18 13:20:06 -0500

    Update VendorDeps (AdvKit, Phoenix6, PhotonLib)

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "milobot";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 5;
-  public static final String GIT_SHA = "80c9e934075dc63f1cb4687a81e3e83603b7c7a4";
-  public static final String GIT_DATE = "2025-01-13 20:33:02 EST";
+  public static final int GIT_REVISION = 6;
+  public static final String GIT_SHA = "f5e35a97d4f4672ce54e78a39f75ebf4b6ed6081";
+  public static final String GIT_DATE = "2025-01-15 17:46:44 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-01-13 20:50:35 EST";
-  public static final long BUILD_UNIX_TIME = 1736819435413L;
+  public static final String BUILD_DATE = "2025-01-18 13:17:50 EST";
+  public static final long BUILD_UNIX_TIME = 1737224270523L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
