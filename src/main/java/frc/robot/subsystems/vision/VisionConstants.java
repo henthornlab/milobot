0d56772fd560ae2dab2e24192b5189c691b051e5
Henthorn Lab @ RHIT <henthor1@rose-hulman.edu>
2025-02-11 19:00:44 -0500

    forced 2025 field

--- src/main/java/frc/robot/subsystems/vision/VisionConstants.java
+++ src/main/java/frc/robot/subsystems/vision/VisionConstants.java
@@ -21,7 +21,8 @@ import edu.wpi.first.math.geometry.Transform3d;
 public class VisionConstants {
   // AprilTag layout
   public static AprilTagFieldLayout aprilTagLayout =
-      AprilTagFieldLayout.loadField(AprilTagFields.kDefaultField);
+      AprilTagFieldLayout.loadField(AprilTagFields.k2025Reefscape);
+
 
   // Camera names, must match names configured on coprocessor
   public static String camera0Name = "photoncamera_5";
